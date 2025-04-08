4e1ba332265fa16bc8c493a9a456ca65601674df
KesharaGunathilaka <gpkhgunathilaka@gmail.com>
2025-04-08 23:34:11 +0530

    Cloudinary update
    File share upload profile pic update
    using cloudinary

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 const Origin = "http://localhost:5173" || process.env.ORIGIN;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
